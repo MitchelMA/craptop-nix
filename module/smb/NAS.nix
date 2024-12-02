f7a0290a8d36cbffd96a07b774c0f8872f5fcc02
Mitchel Vonk <vonkm124@gmail.com>
2024-12-02 16:46:34 +0100

    toevoeging van hard-config en verplaatsing van de samba credentials

--- module/smb/NAS.nix
+++ module/smb/NAS.nix
@@ -5,6 +5,6 @@
     options = let automount_opts =
       "x-systemd.automount,noauto,x-systemd.idle.timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
-    in ["${automount_opts},credentials=/home/mitchel/samba-cred/NAS-secrets,uid=mitchel"];
+    in ["${automount_opts},credentials=/etc/nixos/secrets/smb/NAS-secrets,uid=mitchel"];
   };
 }
